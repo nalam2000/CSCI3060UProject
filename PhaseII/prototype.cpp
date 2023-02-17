46156bc1aeda070839c5bbb3e2a543dbf3914478
nalam2000 <nalam8493@gmail.com>
2023-02-17 18:56:57 -0500

    did some more code

--- PhaseII/prototype.cpp
+++ PhaseII/prototype.cpp
@@ -10,20 +10,17 @@ class User {       // The class
     string Password;  // Attribute 
     void isAuth(string Username, string Password); //Function (bool function which will authenticate user login)
     void Delete();
-};
-
-class Bid {       // The class
-  public:             // Access specifier
-    string itemName;        // Attribute 
-    double bidPrice;  // Attribute 
-    double bidExpiry;
-
-    void isAuth(string Username, string Password); //Function (bool function which will authenticate user login)
+    void create();
 };
 
 class Transaction{
   public:
-   string commands[3] = {"Bid","Refund","AddCredit" };
+   string commands[4] = {"Bid","Refund","AddCredit", "Exit" };
+    string itemName;        // Attribute 
+    double bidPrice;  // Attribute 
+    double bidExpiry;
+
+  void checkBidStatus(string Username, string Password); //Function (bool function which will authenticate user login)
    void doCommand(int command);
 };
 
@@ -31,13 +28,24 @@ class Transaction{
 void User::isAuth(string Username, string Password) {
   cout << "User is authenticated"<< endl;
 }
+//TODO: create user by making more user objects 
+void User::create(){}
+//TODO: Be able to delete User by deleting user objects.
+void User::Delete(){}
+//TODO: Display Bid Status for an item placed by user. User will login and navigate to item and check bid status on that item
+void Transaction::checkBidStatus(string Username, string Password){}
 
 //TODO: receive command from user and perform the transaction commands
 void Transaction::doCommand(int command) {
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 4; i++) {
       if(command-1 == i){
             cout <<"You have selected: " <<commands[i]<<endl;
-            break;
+            //TODO: Call a function to do the subsequent command picked.
+        }else{
+          if(command-1 == 4){
+             cout <<"You have decided to logout. Goodbye."<<endl;
+
+          }
         }
   }
  
@@ -45,10 +53,12 @@ void Transaction::doCommand(int command) {
  
 int main()
 {
+  //TODO: implement main function/program in a loop until user decides to exit.
     User user1;
     string outputloginmsg = "Welcome User! Please log back in to get started";
-    Transaction d;
+    Transaction item;
     int userinput;
+    
  
     cout << outputloginmsg <<" -----------------------------------------------------------------------" <<endl;
  
@@ -60,17 +70,12 @@ int main()
     // Call isauth to verify user. If User is authenticated move to next set of input/output statements
     user1.isAuth(user1.Password, user1.Username);
 
-    cout << "Enter a transaction command(Bid: 1 , Refund: 2, AddCredit: 3): ";
+    cout << "Enter a transaction command(Bid: 1 , Refund: 2, AddCredit: 3, Exit: 4): ";
     cin >> userinput;
-    d.doCommand(userinput);
 
-
+    // Perform commands inputed by user
+    item.doCommand(userinput);
    
-
-
-
-
-
 
 
     return 0;
