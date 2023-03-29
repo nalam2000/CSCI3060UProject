79b95de421bb529534ecd80e2c1235d0e20cf3bb
isan150 <60987496+isan150@users.noreply.github.com>
2023-03-29 16:28:50 -0400

    CurrentUserAccounts Test Classes
    Soo I'm not really sure about the values we should plug into these tests. We should probably work on the actual methods for these to work.

--- SQAPhase4/src/test/CurrentUserAccountsTest.java
+++ SQAPhase4/src/test/CurrentUserAccountsTest.java
@@ -1,4 +1,6 @@
 // Importing some dependencies for testing
+import java.beans.Transient;
+
 import org.junit.jupiter.api.Test;
 
 
@@ -14,6 +16,34 @@ class CurrentUserAccountsTest
 
         // Specify expected results when running the test, and a comparison will be made when this runs.
         assertEquals(expected: "Username", addedUser.addUser());            // Not sure about parameters yet, we're missing logic.
+    }
+
+    // A "testing" method to test the delete user function from CurrentUserAccounts.java
+    @Test
+    void deletingUser()
+    {
+        var deletedUser = new CurrentUserAccounts();
+
+        // Specify expected results when running the test, and a comparison will be made when this runs.
+    }
+
+    // A "testing" method to test the add credit function from CurrentUserAccounts.java
+    @Test
+    void addingCredit()
+    {
+        var addedCredit = new CurrentUserAccounts();
+
+        // Specify expected results when running the test, and a comparison will be made when this runs.
+
+    }
+
+    // A "testing" method to test the refund function from CurrentUserAccounts.java
+    @Test
+    void refundedTransaction()
+    {
+        var refundedAmount = new CurrentUserAccounts();
+
+        // Specify expected results when running the test, and a comparison is made when this runs.
     }
 
 }
