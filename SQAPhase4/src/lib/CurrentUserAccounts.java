79f64519a7d2ddaecefd467250e11c0662cc0500
ShaneFrancis27 <shane.francis@ontariotechu.net>
2023-03-29 22:49:44 -0400

    Add/DeleteUser and File I/O Implementation
    Added Functions to Read and Write to files. And completed logic for add/deleteUser

--- SQAPhase4/src/lib/CurrentUserAccounts.java
+++ SQAPhase4/src/lib/CurrentUserAccounts.java
@@ -1,19 +1,89 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CurrentUserAccounts {
-    // Add attributes for managing user accounts
+    private String mainUserAccount;
 
-    public void addUser() {
-        // Add user creation logic here
+    public CurrentUserAccounts(String mainAcc){
+        this.mainUserAccount = mainAcc;
     }
 
-    public void deleteUser() {
-        // Add user deletion logic here
+    public int addUser(String username, String accountType, float initialCredit){
+        try {
+            List<String[]> accountList = readAccountData();
+            for (String[] account : accountList) {
+                if (account[0].equals(username)) {
+                    System.out.println("ERROR: User already exists");
+                    return -1;
+                }
+            }
+
+            String formattedCredit = String.format("%.2f", initialCredit);
+            while (formattedCredit.length() < 9) {
+                formattedCredit = "0" + formattedCredit;
+            }
+            accountList.add(new String[]{username, accountType, formattedCredit});
+            writeAccountData(accountList);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+        return 0;
     }
 
-    public void addCredit() {
-        // Add credit addition logic here
+    public int deleteUser(String username){
+        try {
+            List<String[]> accountList = readAccountData();
+            List<String[]> updatedAccounts = new ArrayList<>();
+            for (String[] account : accountList) {
+                if (!account[0].equals(username)) {
+                    updatedAccounts.add(account);
+                }
+            }
+            writeAccountData(updatedAccounts);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+        return 0;
     }
 
-    public void refund() {
-        // Add refund processing logic here
+    public int addCredit(){
+        
+        return 0;
     }
+
+    public int refund(){
+        return 0;
+    }
+
+    private List<String[]> readAccountData() throws IOException {
+        List<String[]> accounts = new ArrayList<>();
+        try (FileInputStream fis = new FileInputStream(mainUserAccount);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(fis))) {
+    
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] accData = new String[3];
+                accData[0] = line.substring(0, 15);
+                accData[1] = line.substring(16, 18);
+                accData[2] = line.substring(19, 28);
+                accounts.add(accData);
+            }
+        }
+        return accounts;
+    }
+    
+    private void writeAccountData(List<String[]> accountList) throws IOException {
+        try (FileWriter fw = new FileWriter(mainUserAccount, false);
+             BufferedWriter bw = new BufferedWriter(fw)) {
+            for (String[] account : accountList) {
+                String accountLine = String.join(" ", account);
+                bw.write(accountLine);
+                bw.newLine();
+            }
+        }
+    }
+    
 }
