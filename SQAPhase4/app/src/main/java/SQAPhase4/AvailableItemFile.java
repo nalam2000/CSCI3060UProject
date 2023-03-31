9846bf4b12673c06748704b57203b9834867d59d
nalam2000 <nalam8493@gmail.com>
2023-03-31 16:44:57 -0400

    added comments

--- SQAPhase4/app/src/main/java/SQAPhase4/AvailableItemFile.java
+++ SQAPhase4/app/src/main/java/SQAPhase4/AvailableItemFile.java
@@ -2,22 +2,28 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.List;
+/*
+ In the Available Item Class, We read from "availableitems.txt" and update the file accordingly after the methods in the class 
+ has been invoked.
+ */
 
 public class AvailableItemFile {
     // Add attributes for managing available items
     private static String filePath = "availableitems.txt";
 
+    // Updates Bid in txt file
     public static void bid(String seller, String itemName, String buyer, String bidAmount) throws Exception {
         List<String> lines = Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8);
         StringBuilder sb = new StringBuilder();
-
+        // Read and parse each line
         for (String line : lines) {
             String[] splitString = line.split("\\s+");
             String currentItemName = splitString[0];
             String currentSellerName = splitString[1];
             String currentDaysLeft = splitString[3];
 
-            // if both seller name and item name match, append updated line to string builder
+            // if both seller name and item name match, append updated line to string
+            // builder
             if (seller.equals(currentSellerName) && itemName.equals(currentItemName)) {
                 sb.append(addSpaces(itemName, 20))
                         .append(addSpaces(seller, 16))
@@ -30,12 +36,14 @@ public class AvailableItemFile {
             }
         }
 
-        Files.write(Paths.get(filePath), sb.toString().getBytes(StandardCharsets.UTF_8), StandardOpenOption.TRUNCATE_EXISTING);
+        Files.write(Paths.get(filePath), sb.toString().getBytes(StandardCharsets.UTF_8),
+                StandardOpenOption.TRUNCATE_EXISTING);
 
-        // Add bid placing logic here
     }
 
-    public static void advertise(String itemName, String sellerName, String buyerName, String daysLeft, String startingBid) throws Exception {
+    // Updates product ad in txt file
+    public static void advertise(String itemName, String sellerName, String buyerName, String daysLeft,
+            String startingBid) throws Exception {
         String str = addSpaces(itemName, 20)
                 + addSpaces(sellerName, 16)
                 + addSpaces(buyerName, 16)
@@ -45,18 +53,20 @@ public class AvailableItemFile {
 
         Files.write(Paths.get(filePath), str.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
 
-        // Add advertisement creation logic here
     }
 
+    // Helps formatting txt file and adds spaces
     public static String addSpaces(String str, int numChars) {
         return String.format("%-" + numChars + "s", str);
-        // Add space adding logic here
+
     }
 
+    // Helps formatting txt file and formats the amount for product
     public static String formatAmount(String str) {
         return String.format("%06d", Integer.parseInt(str));
     }
 
+    // Helps formatting txt file and formats remaining days left for product
     public static String formatDaysLeft(String str) {
         return String.format("%03d", Integer.parseInt(str));
     }
--- SQAPhase4/app/src/test/java/SQAPhase4/AvailableItemFileTest.java
+++ SQAPhase4/app/src/test/java/SQAPhase4/AvailableItemFileTest.java
@@ -1,5 +1,6 @@
 package test;
 
+// Imports
 import lib.AvailableItemFile;
 import org.junit.jupiter.api.Test;
 import java.util.Arrays;
@@ -7,6 +8,7 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+// Test Class for testing the Available Item Class. Uses J-unit tests to check statement coverage.
 public class AvailableItemFileTest {
 
     @Test
@@ -18,15 +20,15 @@ public class AvailableItemFileTest {
         // Test case with a non-existing item
         AvailableItemFile.bid("Seller2", "Item2", "Buyer2", "200");
         assertEquals(Arrays.asList("Item1          Seller1        Buyer1        010100",
-                                    "Item3          Seller3        Buyer3        020200",
-                                    "Item4          Seller4        Buyer4        030300",
-                                    "Item5          Seller5        Buyer5        040400",
-                                    "Item6          Seller6        Buyer6        050500",
-                                    "Item7          Seller7        Buyer7        060600",
-                                    "Item8          Seller8        Buyer8        070700",
-                                    "Item9          Seller9        Buyer9        080800",
-                                    "Item10         Seller10       Buyer10       090900",
-                                    "Item11         Seller11       Buyer11       101000"), getAllLines());
+                "Item3          Seller3        Buyer3        020200",
+                "Item4          Seller4        Buyer4        030300",
+                "Item5          Seller5        Buyer5        040400",
+                "Item6          Seller6        Buyer6        050500",
+                "Item7          Seller7        Buyer7        060600",
+                "Item8          Seller8        Buyer8        070700",
+                "Item9          Seller9        Buyer9        080800",
+                "Item10         Seller10       Buyer10       090900",
+                "Item11         Seller11       Buyer11       101000"), getAllLines());
     }
 
     @Test
@@ -34,19 +36,20 @@ public class AvailableItemFileTest {
         // Test case with a new advertisement
         AvailableItemFile.advertise("New Item", "New Seller", "New Buyer", "999", "999");
         assertEquals(Arrays.asList("Item1          Seller1        Buyer1        010100",
-                                    "Item3          Seller3        Buyer3        020200",
-                                    "Item4          Seller4        Buyer4        030300",
-                                    "Item5          Seller5        Buyer5        040400",
-                                    "Item6          Seller6        Buyer6        050500",
-                                    "Item7          Seller7        Buyer7        060600",
-                                    "Item8          Seller8        Buyer8        070700",
-                                    "Item9          Seller9        Buyer9        080800",
-                                    "Item10         Seller10       Buyer10       090900",
-                                    "Item11         Seller11       Buyer11       101000",
-                                    "New Item       New Seller    New Buyer    999999"), getAllLines());
+                "Item3          Seller3        Buyer3        020200",
+                "Item4          Seller4        Buyer4        030300",
+                "Item5          Seller5        Buyer5        040400",
+                "Item6          Seller6        Buyer6        050500",
+                "Item7          Seller7        Buyer7        060600",
+                "Item8          Seller8        Buyer8        070700",
+                "Item9          Seller9        Buyer9        080800",
+                "Item10         Seller10       Buyer10       090900",
+                "Item11         Seller11       Buyer11       101000",
+                "New Item       New Seller    New Buyer    999999"), getAllLines());
     }
 
     @Test
+    // Test Helper function Add Spaces and assert whether it works
     public void testAddSpaces() {
         assertEquals("abc             ", AvailableItemFile.addSpaces("abc", 16));
         assertEquals("def             ", AvailableItemFile.addSpaces("def", 16));
@@ -54,6 +57,8 @@ public class AvailableItemFileTest {
     }
 
     @Test
+    // Test Helper function Format amount and assert whether the functions it works
+    // the way it is intended.
     public void testFormatAmount() {
         assertEquals("000100", AvailableItemFile.formatAmount("100"));
         assertEquals("012345", AvailableItemFile.formatAmount("12345"));
@@ -61,6 +66,8 @@ public class AvailableItemFileTest {
     }
 
     @Test
+    // Test Helper function FormatDaysLeft and assert whether it works the way it is
+    // intended.
     public void testFormatDaysLeft() {
         assertEquals("001", AvailableItemFile.formatDaysLeft("1"));
         assertEquals("050", AvailableItemFile.formatDaysLeft("50"));
