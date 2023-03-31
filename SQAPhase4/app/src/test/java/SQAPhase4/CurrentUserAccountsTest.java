2e9356be902c31c484f050939d47db298268bb2f
ShaneFrancis27 <shane.francis@ontariotechu.net>
2023-03-31 16:27:08 -0400

    Path Coverage Tests

--- SQAPhase4/app/src/test/java/SQAPhase4/CurrentUserAccountsTest.java
+++ SQAPhase4/app/src/test/java/SQAPhase4/CurrentUserAccountsTest.java
@@ -71,4 +71,31 @@ public class CurrentUserAccountsTest {
         currentUserAccounts.addUser("buyer", "01", 5000.0f);
         assertEquals(0, currentUserAccounts.refund("seller", "buyer", 1000.0f));
     }
+
+
+    // Path Coverage Tests 
+     // Test case: Add credit to an existing user without exceeding the credit limit
+    @Test
+    void testAddCreditUserExistsAndNotExceedLimit() {
+        // Add a user with an initial credit amount
+        currentUserAccounts.addUser("testuser", "01", 5000.0f);
+        // Attempt to add credit within the allowed limit
+        assertEquals(0, currentUserAccounts.addCredit("testuser", 1000.0f));
+    }
+
+    // Test case: Add credit to an existing user, but exceed the credit limit
+    @Test
+    void testAddCreditUserExistsAndExceedLimit() {
+        // Add a user with an initial credit amount close to the limit
+        currentUserAccounts.addUser("testuser", "01", 995000.0f);
+        // Attempt to add credit that would exceed the allowed limit
+        assertEquals(-1, currentUserAccounts.addCredit("testuser", 10000.0f));
+    }
+
+    // Test case: Attempt to add credit to a non-existent user
+    @Test
+    void testAddCreditUserNotExists() {
+        // Attempt to add credit to a user that doesn't exist in the system
+        assertEquals(-1, currentUserAccounts.addCredit("nonexistent", 1000.0f));
+    }
 }
