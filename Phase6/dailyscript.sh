245a5b4eef55cfc268f2c57197b616ebcbe7e064
nalam2000 <nalam8493@gmail.com>
2023-04-10 14:21:57 -0400

    added test script for input/output

---
+++ Phase6/dailyscript.sh
@@ -0,0 +1,23 @@
+runFrontEnd="./FrontEnd1/a.out"
+transactions="./DailyScriptTransactions/$1"
+
+echo "$1"
+
+# run 5 frontend sessions
+for i in DailyInputs/$1/*
+do
+  echo "$i"
+  filename=$(basename $i)
+  rm $transactions/$filename
+  $runFrontEnd currentusers.txt availableitems.txt $transactions/$filename < $i &> /dev/null
+done
+
+# combine transaction files into one daily transaction file
+rm dailytransactions.txt
+for i in $transactions/*
+do
+  cat $i >> dailytransactions.txt
+done
+
+# run backend with daily transactions
+java -jar backend.jar
---
+++ Phase6/weeklyscript.sh
@@ -0,0 +1,26 @@
+frontend="./Frontend1"
+
+# compile frontend
+g++ $frontend/main.cpp
+
+# clear previous content in files
+: > currentusers.txt
+: > availableitems.txt
+
+# set file content
+text="fullUsername    FS 001110.00
+buyUsername     BS 009999.99
+sellUsername    SS 000000.00
+adminUsername   AA 000050.00"
+echo "$text" > currentusers.txt
+
+text="basketball          someUs3r        fullUsername    010.00
+sneakers            exampleUsername someBidder      005.00"
+echo "$text" > availableitems.txt
+
+# run the daily script 5 times
+for i in {1..5}
+do
+	foldername="day$i"
+	./dailyscript.sh $foldername
+done
